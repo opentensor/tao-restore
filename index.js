cee0afd22a8bcba097cfe33f2864e3e1cd15c9a9
Cameron Fairchild <cameron@opentensor.ai>
2024-09-12 13:39:48 -0400

    add batching

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { ApiPromise, WsProvider } from "@polkadot/api"
-import { readFileSync, writeFileSync } from "fs"
-import { Keyring } from '@polkadot/keyring';
-import { waitReady } from '@polkadot/wasm-crypto';
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { readFileSync, writeFileSync } from "fs";
+import { Keyring } from "@polkadot/keyring";
+import { waitReady } from "@polkadot/wasm-crypto";
 
-const lite_node = "wss://lite.chain.opentensor.ai:443"
-const provider = new WsProvider(lite_node)
-const api = new ApiPromise({ provider: provider })
+const lite_node = "wss://lite.chain.opentensor.ai:443";
+const provider = new WsProvider(lite_node);
+const api = new ApiPromise({ provider: provider });
 
-const emit_map = readFileSync("emit_map.json", "utf-8")
-const emit_map_json = JSON.parse(emit_map)
+const emit_map = readFileSync("emit_map.json", "utf-8");
+const emit_map_json = JSON.parse(emit_map);
 
 // TODO: Uncomment and fill in mnemonic
 // const mnemonic = "your mnemonic here"
@@ -17,33 +17,49 @@ const emit_map_json = JSON.parse(emit_map)
 // const OWNER_KEY = "your owner key here"
 
 const main = async (emit_map_json) => {
-    await waitReady()
+  await waitReady();
 
-    // const wallet_key = new Keyring({ type: 'sr25519' }).addFromMnemonic(mnemonic)
+  // const wallet_key = new Keyring({ type: 'sr25519' }).addFromMnemonic(mnemonic)
 
-    const pub_key = new Keyring({ type: 'sr25519' }).addFromAddress(OWNER_KEY)
-    
-    await api.isReady
+  const pub_key = new Keyring({ type: "sr25519" }).addFromAddress(OWNER_KEY);
 
-    let batch_calls = [];
-    Object.keys(emit_map_json).forEach(async (key) => {
-        let to_emit = emit_map_json[key]
-        let tx = api.tx.balances.transferKeepAlive(
-            key, to_emit
-        )
-        batch_calls.push(tx)
-    })
-    
-    let batch_call = api.tx.utility.batch(batch_calls)
-    let fee_estimate = await batch_call.paymentInfo(pub_key)
+  await api.isReady;
 
-    console.log("Fee Estimate: ", fee_estimate.partialFee.toHuman())
+  let batches = [];
+  let batch_calls = [];
+  let curr_batch_size = 0;
+  console.log("Creating batches of calls");
+  Object.keys(emit_map_json).forEach(async (key) => {
+    if (curr_batch_size >= 1000) {
+      // Batch into 1000 txs for block size reduction
+      batches.push(batch_calls);
+      batch_calls = [];
+      curr_batch_size = 0;
+    }
 
-    // TODO: Choose one of two options, sign and send or write to file
-    // const txHash = await batch_call.signAndSend(wallet_key)
-    // console.log(`Submitted with hash ${txHash}`);
+    let to_emit = emit_map_json[key];
+    let tx = api.tx.balances.transferKeepAlive(key, to_emit);
+    batch_calls.push(tx);
+    curr_batch_size += 1;
+  });
 
-    // writeFileSync("batch_call_js.hex", batch_call.toHex())
-}
+  if (batches.length > 0) {
+    console.log("Creating batch calls");
+    for (const [i, batch] of batches.entries()) {
+      let batch_call = api.tx.utility.batch(batch);
+      let fee_estimate = await batch_call.paymentInfo(pub_key);
 
-await main(emit_map_json)
+      console.log("Fee Estimate: ", fee_estimate.partialFee.toHuman());
+
+      // TODO: Choose ONE of two options, sign and send or write to file
+      // const txHash = await batch_call.signAndSend(wallet_key)
+      // console.log(`Submitted batch:${i} with hash ${txHash}`);
+
+      // writeFileSync(`batch_call_js_${i}.hex`, batch_call.toHex());
+    }
+  }
+
+  console.log("Done");
+};
+
+await main(emit_map_json);
